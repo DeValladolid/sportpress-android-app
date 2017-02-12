357e8c296af8ec06bc7e0e2fd8913ae36a7b7586
DeValladolid <devalladolidoficial@gmail.com>
2017-02-12 13:57:38 +0100

    1st integration of the Rest Api in the app.

--- app/src/main/java/com/wordpressappexample/HorizontalNtbActivity.java
+++ app/src/main/java/com/wordpressappexample/HorizontalNtbActivity.java
@@ -9,14 +9,103 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.List;
+import java.util.Map;
 
 import devlight.io.library.ntb.NavigationTabBar;
 
 import java.util.ArrayList;
 
 /**
- * Created by GIGAMOLE on 28.03.2016.
+ * Created by DeValladolid on 12.02.2017
  */
+public class HorizontalNtbActivity extends AppCompatActivity {
+
+    String url = "http://www.hojiblanca.com/wp-json/wp/v2/posts?per_page=10";
+    List<Object> list;
+    Gson gson;
+    ProgressDialog progressDialog;
+    ListView postList;
+    Map<String, Object> mapPost;
+    Map<String, Object> mapTitle;
+    int postID;
+    String postTitle[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horizontal_ntb);
+
+        postList = (ListView) findViewById(R.id.postList);
+
+        progressDialog = new ProgressDialog(activity_horizontal_ntb.this);
+        progressDialog.setMessage("Cargando...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
+        StringRequest myRequest = new StringRequest(Request.Method.GET, url,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        gson = new Gson();
+                        list = (List) gson.fromJson(response, List.class);
+                        postTitle = new String[list.size()];
+
+                        for (int i = 0; i < list.size(); ++i) {
+                            mapPost = (Map<String, Object>) list.get(i);
+                            mapTitle = (Map<String, Object>) mapPost.get("title");
+                            postTitle[i] = (String) mapTitle.get("rendered");
+                        }
+
+                        postList.setAdapter(new ArrayAdapter(activity_horizontal_ntb.this,android.R.layout.simple_list_item_1,postTitle));
+                        progressDialog.dismiss();
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(activity_horizontal_ntb.this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(myRequest);
+
+        postList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mapPost = (Map<String,Object>)list.get(position);
+                postID = ((Double)mapPost.get("id")).intValue();
+
+                Intent i = new Intent(getApplicationContext(), Post.class);
+                i.putExtra("id", ""+postID);
+                startActivity(i);
+            }
+        });
+    }
+}
+
+
 public class HorizontalNtbActivity extends Activity {
 
     @Override
